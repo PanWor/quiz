041e3bb6de0f688fefcb9ba7a215425fbb03496f
PanWor <97786623+PanWor@users.noreply.github.com>
2024-04-11 17:34:30 +0200

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Jaką nazwę nosi zdarzenie JavaScript, będące reakcją na pojedynczo kliknięty dowolny element strony?",
+    answer: "onClick",
+    options: [
+      "onKeyDown",
+      "onDblClick",
+      "onClick",
+      "onLoad"
+    ]
+  },
+    {
+    numb: 2,
+    question: "W języku skryptowym JavaScript zmienne mogą być deklarowane:",
+    answer: "w momencie pierwszego użycia zmiennej",
+    options: [
+      "w momencie pierwszego użycia zmiennej",
+      "tylko jeśli podamy typ zmiennej i jej nazwę",
+      "tylko na początku skryptu",
+      "zawsze z poprzedzającym nazwę znakiem $"
+    ]
+  },
+    {
+    numb: 3,
+    question: "W języku JavaScript poprawnie zadeklarowana zmienna to:",
+    answer: "haslo2",
+    options: [
+      "#hasło",
+      "haslo2",
+      "$haslo2",
+      "hasło%"
+    ]
+  },
+    {
+    numb: 4,
+    question: "W języku JavaScript, aby sprawdzić warunek czy liczba znajduje się w przedziale (100;200>, należy zapisać:",
+    answer: "if (liczba > 100 && liczba <= 200)",
+    options: [
+      "if (liczba > 100 && liczba <= 200)",
+      "if (liczba > 100 || liczba <= 200)",
+      "if (liczba < 100 && liczba <= 200)",
+      "if (liczba < 100 || liczba >= 200)"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Jakie zadanie ma metoda document.getElementById(id) w języku JavaScript:",
+    answer: "zwrócić odniesienie do pierwszego elementu HTML o podanym id",
+    options: [
+      "pobrać dane z pola formularza i wstawić je do zmiennej id",
+      "wstawić tekst o treści ’id’ na stronie WWW",
+      "zwrócić odniesienie do pierwszego elementu HTML o podanym id",
+      "sprawdzić poprawność formularza o identyfikatorze id"
+    ]
+  }
+];
